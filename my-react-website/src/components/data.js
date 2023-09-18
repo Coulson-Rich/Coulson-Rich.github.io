cdfda586fdd32216b1f6a58d86a77cb7ffa6a66f
Coulson Rich <coulsonrich5@gmail.com>
2023-09-17 18:57:56 -0600

    Projects Done

--- my-react-website/src/components/data.js
+++ my-react-website/src/components/data.js
@@ -1,36 +1,29 @@
 export const projects = [
-    // {
-    //   title: "Family Map",
-    //   subtitle: "Java Server and Android Client",
-    //   description:
-    //     "This project uses Java as a front and backend. It uses a SQL database to store and retrieve data. It also uses Google Maps API to display the data.",
-    //   //image: "./project-1.gif",
-    //   //link: "https://reactbootcamp.com",
-    // },
-    // {
-    //   title: "Twitter Clone",
-    //   subtitle: "Java Frontend, AWS Server, and DynamoDB Database",
-    //   description:
-    //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    //   //image: "./project-2.gif",
-    //   //link: "https://reedbarger.com",
-    // },
-    // {
-    //   title: "DevChat",
-    //   subtitle: "React and Firebase",
-    //   description:
-    //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    //   //image: "./project-3.gif",
-    //   //link: "https://jsbootcamp.com",
-    // },
-    // {
-    //   title: "Epic Todo App",
-    //   subtitle: "React Hooks",
-    //   description:
-    //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    //   //image: "./project-4.gif",
-    //   //link: "https://pythonbootcamp.com",
-    // },
+    {
+      title: "Family Map",
+      subtitle: "Java Server and Android Client. Click for GitHub",
+      description:
+        "This project uses Java as a front and backend. It uses a SQL database to store and retrieve data. It also uses Google Maps API to display the data.",
+      image: "./FamilyMapExample.gif",
+      link: "https://github.com/Coulson-Rich",
+    },
+    {
+      title: "Colorization of Black and White Movies. .",
+      subtitle: "Python and PyTorch. Click for semi-formal summary",
+      description:
+        "This project uses Python and PyTorch to colorize black and white movies. It uses a convolutional neural network to colorize each frame.",
+      image: "./1087frame.jpg",
+      link: "https://docs.google.com/document/d/e/2PACX-1vQRDCCxVYjZ0ggKAaG4aS0EnGrxPY9d2RxbzKG-AeYwwI5otTqjKGyoW-kcc1_ApsyOaSTI6VlZlbTx/pub",
+    },
+    {
+      title: "Twitter Clone",
+      subtitle: "Java Frontend, AWS Server, and DynamoDB Database\nClick for GitHub",
+      description:
+        "A Twitter clone that uses Java as a frontend and AWS services to host the database, lambda functions, and APIs.",
+      image: "./Tweeter.gif",
+      link: "https://github.com/Coulson-Rich",
+    },
+    
   ];
   
   export const skills = [
--- my-react-website/src/components/projects.js
+++ my-react-website/src/components/projects.js
@@ -12,21 +12,19 @@ export default function Projects() {
             Apps I've Built
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
-            facilis repellat ab cupiditate alias vero aliquid obcaecati quisquam
-            fuga dolore.
+            A curated list of some of my favorite projects I've worked on.<br/>Hover for more project info.
           </p>
         </div>
-        <div className="flex flex-wrap -m-4">
+        <div className="flex flex-wrap -m-4 brightness-90">
           {projects.map((project, index) => (
-            <div //changed from a because i dont know what links ill use yet
-              //href={project.link}
-              //key={project.image}
-              className="sm:w-1/2 w-100 p-4">
-              <div className="flex relative">
+            <a 
+              href={project.link}
+              key={project.image}
+              className="sm:w-1/3 w-100 p-4">
+              <div className="flex relative" style={{ height: '700px' }}>
                 <img
                   alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
+                  className="absolute inset-0 text-center w-full h-full object-cover object-center"
                   src={project.image}
                 />
                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
@@ -39,7 +37,7 @@ export default function Projects() {
                   <p className="leading-relaxed">{project.description}</p>
                 </div>
               </div>
-            </div>
+            </a>
           ))}
         </div>
       </div>
